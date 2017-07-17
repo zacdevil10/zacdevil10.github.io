36e6e473886a4bd61b286621caa106e840cd1800
zacdevil10 <zac1999ad@gmail.com>
2017-07-17 14:16:12 +0100

    Layout for about me

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
-angular.module().controller();
+angular.module('jobsView', []).controller('jobController', function ($scope) {
+  $scope.jobs = [
+    {
+      'name': 'STUDI/O',
+      'link': 'https://appsbystudio.co.uk',
+      'desc': 'This is a compnay that makes android apps and other things. This is a compnay that makes android apps and other things. This is a compnay that makes android apps and other things. This is a compnay that makes android apps and other things. This is a compnay that makes android apps and other things.  This is a compnay that makes android apps and other things.'
+    },
+    {
+      'name': 'company 2',
+      'link': 'https://google.com',
+      'desc': 'This is a template for layout purposes'
+    },
+    {
+      'name': 'company 3',
+      'link': 'https://google.com',
+      'desc': 'This is a template for layout purposes'
+    },
+    {
+      'name': 'company 4',
+      'link': 'https://google.com',
+      'desc': 'This is a template for layout purposes'
+    },
+    {
+      'name': 'company 5',
+      'link': 'https://google.com',
+      'desc': 'This is a template for layout purposes'
+    }
+  ];
+});
