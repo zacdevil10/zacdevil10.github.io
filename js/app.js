92f63abba48fc5d6bc6433aabedb5511962f96fb
zacdevil10 <zac1999ad@gmail.com>
2017-07-22 16:43:23 +0100

    Message logo was no showing up due to cases

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ angular.module('appView', []).controller('appController', function ($scope) {
     },
     {
       'name': 'Message+',
-      'img': '../img/message+ logo.png',
+      'img': '../img/message.png',
       'playlink': '',
       'gitlink': 'https://github.com/zacdevil10/Message-',
       'desc': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
