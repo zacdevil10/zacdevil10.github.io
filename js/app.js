2a127601f42363f803da543df5f794bf902a9712
zacdevil10 <zac1999ad@gmail.com>
2017-07-22 15:46:20 +0100

    General layout for portfolio page. Still to add pictures.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,29 @@
-angular.module('jobsView', []).controller('jobController', function ($scope) {
-  $scope.jobs = [
+angular.module('appView', []).controller('appController', function ($scope) {
+  $scope.apps = [
     {
-      'name': 'STUDI/O',
-      'link': 'https://appsbystudio.co.uk',
-      'desc': 'STUDI/O is a small web-based software development company founded in 2016 by Zac Hadjineophytou and Joshua Tyler-Jones. Its current aim is to develop user friendly android apps that make every day tasks easier.'
+      'name': 'GeoShare',
+      'img': '../img/geoshare.png',
+      'playlink': 'https://play.google.com/store/apps/details?id=uk.co.appsbystudio.geoshare&hl=en',
+      'gitlink': 'https://github.com/STUDIO-apps/GeoShare_Android',
+      'desc': 'Geoshare is an app. Put the google play description here?',
+      'tags': [{"tag":'android', "tag":'Java', "tag":'android-studio'}]
+    },
+    {
+      'name': 'Connect',
+      'img': '../img/geoshare.png',
+      'playlink': '',
+      'gitlink': 'https://github.com/STUDIO-apps/Connect_Android',
+      'desc': 'Connect is an app. Put the google play description here?',
+      'tags': [{"tag":'android', "tag":'Java', "tag":'android-studio'}]
+    },
+    {
+      'name': 'Message+',
+      'img': '../img/message+ logo.png',
+      'playlink': '',
+      'gitlink': 'https://github.com/zacdevil10/Message-',
+      'desc': 'Message+ is an app. Put the google play description here?',
+      'tags': [{"tag":'android', "tag":'Java', "tag":'android-studio'}]
     }
   ];
+
 });
