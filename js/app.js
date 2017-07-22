20b89eb18fd2b8605728f92ee47318d3e3f7599f
zacdevil10 <zac1999ad@gmail.com>
2017-07-22 16:41:47 +0100

    Portfolio uses img in place of the title and title has moved to column 2

--- js/app.js
+++ js/app.js
@@ -5,15 +5,15 @@ angular.module('appView', []).controller('appController', function ($scope) {
       'img': '../img/geoshare.png',
       'playlink': 'https://play.google.com/store/apps/details?id=uk.co.appsbystudio.geoshare&hl=en',
       'gitlink': 'https://github.com/STUDIO-apps/GeoShare_Android',
-      'desc': 'Geoshare is an app. Put the google play description here?',
+      'desc': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       'tags': [{"tag":'android', "tag":'Java', "tag":'android-studio'}]
     },
     {
       'name': 'Connect',
-      'img': '../img/geoshare.png',
+      'img': '../img/blueline.png',
       'playlink': '',
       'gitlink': 'https://github.com/STUDIO-apps/Connect_Android',
-      'desc': 'Connect is an app. Put the google play description here?',
+      'desc': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       'tags': [{"tag":'android', "tag":'Java', "tag":'android-studio'}]
     },
     {
@@ -21,7 +21,7 @@ angular.module('appView', []).controller('appController', function ($scope) {
       'img': '../img/message+ logo.png',
       'playlink': '',
       'gitlink': 'https://github.com/zacdevil10/Message-',
-      'desc': 'Message+ is an app. Put the google play description here?',
+      'desc': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       'tags': [{"tag":'android', "tag":'Java', "tag":'android-studio'}]
     }
   ];
