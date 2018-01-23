80734e6c98ac458404006c31fd6777f52a4bf651
Zac Hadjineophytou <zac@zac-h.co.uk>
2018-01-23 15:10:20 +0000

    Update STUDI/O description

--- js/jobs.js
+++ js/jobs.js
@@ -3,7 +3,7 @@ angular.module('jobsView', []).controller('jobController', function ($scope) {
     {
       'name': 'STUDI/O',
       'link': 'https://appsbystudio.co.uk',
-      'desc': 'STUDI/O is a small web-based software development company founded in 2016 by Zac Hadjineophytou and Joshua Tyler-Jones. Its current aim is to develop user friendly android apps that make every day tasks easier.'
+      'desc': 'STUDI/O is a small web-based software development company founded in 2016 by Zac Hadjineophytou and Joshua Tyler-Jones.'
     }
   ];
 });
