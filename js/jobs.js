3531368019825b5e21b44a2f19117c3b5a2b6d60
Zac Hadjineophytou <zac@appsbystudio.co.uk>
2018-11-13 11:09:06 +0000

    Update studio website link address

--- js/jobs.js
+++ js/jobs.js
@@ -2,7 +2,7 @@ angular.module('jobsView', []).controller('jobController', function ($scope) {
   $scope.jobs = [
     {
       'name': 'STUDI/O',
-      'link': 'https://appsbystudio.co.uk',
+      'link': 'https://studio-apps.github.io/',
       'desc': 'STUDI/O is a small web-based software development company founded in 2016 by Zac Hadjineophytou and Joshua Tyler-Jones.'
     }
   ];
