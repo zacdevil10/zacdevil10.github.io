a25281af3afdcab718dbdc1e9546e6230ccf76e1
Zac Hadjineophytou <zac1999ad@gmail.com>
2022-05-21 18:45:30 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -22,35 +22,6 @@ angular.module('appView', []).controller('appController', function ($scope) {
       'tags': [{"tag":'android', "tag":'kotlin', "tag":'xml', "tag":'library'}]
     },
     {
-      'id': 'magic-squares',
-      'name': 'Magic Squares',
-      'img': '../img/magicsquares.png',
-      'color': '#7CB342',
-      'gitlink': 'https://github.com/zacdevil10/MagicSquares',
-      'desc': 'A simple magic squares solver.',
-      'tags': [{"tag":'android', "tag":'kotlin', "tag":'xml'}]
-    },
-    {
-      'id': 'leaptouch',
-      'name': 'Leap Motion Touch Screen [WIP]',
-      'img': '../img/leaptouch.png',
-      'color': '#616161',
-      'gitlink': 'https://github.com/zacdevil10/LeapMotionTouchScreen',
-      'desc': 'Control your mouse with the Leap Motion.',
-      'tags': [{"tag":'java', "tag":'javafx', "tag":'leap-motion'}]
-    },
-    {
-      'id': 'connect',
-      'name': 'Connect [WIP]',
-      'img': '../img/connect.png',
-      'color': '#009688',
-      'website': '',
-      'playlink': '',
-      'gitlink': 'https://github.com/STUDIO-apps/Connect_Android',
-      'desc': 'Currently in the early stages of development, Connect will allow you to make and receive calls from your PC using your computers audio devices, sync and send text messages and get all notifications on your computer.',
-      'tags': [{"tag":'android', "tag":'java'}]
-    },
-    {
       'id': 'geoshare',
       'name': 'GeoShare [ARCHIVE]',
       'img': '../img/geoshare.png',
@@ -59,25 +30,6 @@ angular.module('appView', []).controller('appController', function ($scope) {
       'gitlink': 'https://github.com/zacdevil10/GeoShare_Android',
       'desc': 'GeoShare is a location sharing app.',
       'tags': [{"tag":'android', "tag":'kotlin', "tag":'xml'}]
-    },
-    {
-      'id': 'easybmi',
-      'name': 'EasyBMI [ARCHIVE]',
-      'img': '../img/easybmi.png',
-      'color': '#009688',
-      'gitlink': 'https://github.com/zacdevil10/EasyBMI',
-      'desc': 'EasyBMI is a simple to use BMI calculator.',
-      'tags': [{"tag":'android', "tag":'java'}]
-    },
-    {
-      'id': 'messageplus',
-      'name': 'Message+ [ARCHIVE]',
-      'img': '../img/message.png',
-      'color': '#9c27b0',
-      'playlink': '',
-      'gitlink': 'https://github.com/zacdevil10/Message-',
-      'desc': 'Currently in development, Message+ will be a more feature rich messaging app with features such as a backup and restore option.',
-      'tags': [{"tag":'android', "tag":'java'}]
     }
   ];
 });
