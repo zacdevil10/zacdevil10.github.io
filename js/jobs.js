6fc14090deb0e3806bfadffd33758105076d1772
Zac Hadjineophytou <zac1999ad@gmail.com>
2022-12-28 14:04:40 +0000

    Corrected Atom bank start date

--- js/jobs.js
+++ js/jobs.js
@@ -3,7 +3,7 @@ angular.module('jobsView', []).controller('jobController', function ($scope) {
     {
       'name': 'Atom bank',
       'link': 'https://atombank.co.uk/',
-      'desc': 'Android engineer at Atom bank from 2021 to present.'
+      'desc': 'Android engineer at Atom bank from October 2020 to present.'
     }
   ];
 });
